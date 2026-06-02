6cd04c3a1cf2ee53554e9b57d910d3af01ecd4f0
andreas <andreassklizovic@gmail.com>
2026-06-02 18:38:01 +0200

    5 datoteka

---
+++ UcemjeFD3/app.js
@@ -0,0 +1,13 @@
+// const year = Date.now();
+// const retirementAge = (year, birthAge) => {
+//   const retirement = 65 - (year - birthAge);
+//
+//   return retirement;
+// };
+//
+// console.log(retirementAge(2024, 1962));
+
+
+// v      
+
+ 
