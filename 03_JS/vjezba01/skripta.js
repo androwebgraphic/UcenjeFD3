6110bb5fb885cabea8df46e5bd152862610d7e75
andreas <andreassklizovic@gmail.com>
2026-07-13 19:21:00 +0200

    JS

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,25 @@
+//komentar tocka  zrez moze  i ne mora
+
+//alert('Hello World 1')
+
+/*
+vise lnija komentar
+*/
+
+document.getElementById('ispis').innerText = 'Hello world 6'
+
+document.getElementById('ispis').style.color = 'red'
+
+document.getElementById('ge').innerHTML =
+'<a href="#">ovo je  poveznica</a>'
+
+//bactics
+
+console.log(`Ovaj
+  tex mogu
+  pisat u
+  vise redova
+  i
+  druge pogodnosti`)
+
+  
