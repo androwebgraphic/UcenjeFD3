dd980c5bc2fa3e14ac556eb5028ef5da38fd6f39
andreas <andreassklizovic@gmail.com>
2026-07-15 18:53:52 +0200

    ispravke

--- 03_JS/vjezba03/skripta.js
+++ 03_JS/vjezba03/skripta.js
@@ -1,165 +1,179 @@
-//operatori
+// operatori
 
-// Aritmeticki  operatori
-//+ - * /
+// vjezba02 = == ===
 
-const a = 2, b = 3 // deklariranje 2  varijable  u jednoj liniji
+// aritmetički operatori
+// + - * /
+
+// ovdje može doći prompt s parseInt ili Number - ULAZ
+const a = 2, b = 3 // deklariranje dvije varijable u jednoj liniji
 
 let rez // undefined
-//ovdje  moze doci promp s parseInt ili  Number-Ulaz
-
-//algoritam
+// ALGORITAM
 rez = a + b
-//izlaz
-console.log(rez)
+// IZLAZ
+console.log('rez', rez) // 5
 
+// neću pokazivati primjere za - * /
 
-//modulo (%)
-//ostatak nakon cijelobrojnog dijeljenja !!!!
+// modulo operator (%)
+// ostatak nakon cjelobrojnog djeljenja
 
-//9%2=1
-//4*2 =8
-//9-8=1 ovo je rezultat
+// 9 % 2 = 1
+// 4 * 2 = 8
+// 9 - 8 = 1 <- ovo je rezultat
 
+console.log('9 % 2', 9 % 2) // 1
+console.log('8 % 2', 8 % 2) // 0
 
-//modulo se  koristi pri odredivanju parnosti broja
-rez = a%b
+// modulo se najčešće koristi pri određivanju parnosti broja
 
+// operatori dodjele
 
-console.log('rez, rez')
-console.log('9 % 2', 9%2) //1
-console.log('8 % 2',8%2)//0
+rez += 2 // isto kao i rez = rez + 2
 
-//Operatori dodjele
-rez += 2 //isto ko rez= rez+2
 console.log('rez+=2', rez)
-
 
 //debugger
 rez = rez + 3
 
-console.log('rez=rez+3',rez)
+console.log('rez = rez + 3', rez)
 
-//postoje i -=,*=, /=
-//vjerovali ili ne
-//najcesca u programiranju je uvecanje varijable za 1
+// postoje i -=, *=, /= 
 
-//nacin uvecanja varijable za 1
+// vjerovali ili ne
+// najčešća operacija u programiranju je uvećanje varijable za 1
 
-rez = rez + 1//11
-rez += 1//12
+// način uvećanja varijable za 1
+rez = rez + 1 // 11
+rez += 1 // 12
+rez++   // 13   -> ovaj način zapisa se nikada ne bi trebao koristiti sam u linij
 
-rez++// ovaj nacin nikad koristiti sam u liniji
-
-//increment ++
-//decrement --
-//ima dvojaku ulogu
+// inkrement ++ i dekrement --
+// dvojaku ulogu
+// kada se koristi i kada se uvećava/smanjuje
 
 let i = 0
 
-// prvo koristi pa  uvecaj
-console.log('i++', i++)//0 -> nakon ispisa i=1
+// prvo koristi pa uvećaj
+console.log('i++', i++) // 0    -> nakon ispisa i=1
 
-//prvo uvecaj pa koristi
-console.log('++i', ++i)// 2 -> 1+1 =2 i onda to ispise
+// prvo uvećaj pa koristi
+console.log('++i', ++i) // 2    -> 1 + 1 daje 2 i onda to ispiše
 
-//isto i kod --
+// isto sve vrijedi i za --
+
+// ovo može biti na razgovoru za posao
 //debugger
 i = 2
 let j = 1
-i = j++ - i // 1-2  j=2, i=-1
-j += --i - ++j// i=-2 -3 = -5, j=-2
+i = j++ - i   // 1 - 2        j = 2, i = -1
+j += --i - ++j  // i = -2 - 3 = -5,  j = -3
+console.log(i - j) // 1
 
-console.log(i - j)//-3
 
-//Operatori usporedbe
+// operatori uspoređivanja == ===
 
-//razlicito !=
-
-console.log(5 != 4)//true
-console.log('5' != 5)//false
-console.log('5' !== 5)//true
+// različito !=, !==
+console.log(5 != 4) // true
+console.log('5' != 5) // false
+console.log('5' !== 5) //true
 
 // <, >, <=, >=
-
 const godine = 18
 
-console.log(godine > 18)//false
-console.log(godine >= 18)//true 
+console.log(godine > 18) // false
+console.log(godine >= 18) // true
 
-// logicki operatori -> rade na  logickom tipu podataka: BOOLEAN
-
+// logički operatori -> rade na logičkom tipu podataka: boolean
 const ispunjenUvjet = godine >= 18
 
 console.log('ispunjenUvjet', typeof ispunjenUvjet, ispunjenUvjet)
-// AND, OR i NOT (I, ILI, Ne)
-//&&, ||, i !
+
+// AND, OR i NOT (I, ILI i NE)
+// &&, || (Alt Gr + W) i !     -> U JS se ne smije koristi & na boolean, rezerviran je za operacije nad bitovima
+
 const imaNovaca = true
-console.log('Moze u disko', ispunjenUvjet && imaNovaca)//true
-//vrijed pravila boolovih tablica:
+
+console.log('Može u disko', ispunjenUvjet && imaNovaca) // true
+
+// vrijede pravila booleovih tablica:
+// https://introcs.cs.princeton.edu/java/71boolean/images/truth-table.png
 
 console.log(!imaNovaca)
 
-//ternary
-console.log(godine >= 18 ? 'punoljetan' : 'Maloljetan')
 
-//operator spajanaj - nadoljepljivanja CONCATENATION +
-//+ operator zbrajanaj kod 2  broja
+// ternarni operator ?:
+console.log(godine >= 18 ? 'Punoljetan' : 'Maloljetan')
 
-console.log('Marko ima ' + godine + ' godina')
+
+// operator spajanja - nadoljepljivanja (concatination)
+// +
+// + je operator zbrajanja kada se radi o dva broj
+// u svim ostalim situacijama + spaja
+console.log('Marko ima ' + godine + ' godina') // ovo nije baš najbolje
 
 console.log(`Marko ima ${godine} godina`)
 
-const x='7' , y=5
-console.log(x + y)
 
-//Operator prosirivanja SPREAD
+const x = '7', y = 5
+console.log(x + y) // 75
 
-const niz = [2.1]//konstanta ne  moze se  mijenjati
-const noviNiz = [0, ...niz, 3]
+
+// spread operator - operator proširivanja (...)
+
+const niz = [2, 1] // konstanta koja se ne može mijenjati
+
 console.table(niz)
-console.log(noviNiz)
+
+const noviNiz = [0, ...niz, 3]
+
+console.table(noviNiz)
 
 const osoba = {
-
-  ime: 'Pero',
-  prezime:'Peric'
+    ime: 'Pero',
+    prezime: 'Perić'
 }
 
 const polaznik = {
-  
-  .. osoba,
-  edukacija: 'FD'
+    ...osoba,
+    edukacija: 'FD'
 }
 
-//suprotn od  prosirivanja DEstructing
+// suprotno od priširivanja (...) je destructing operator
 
-const [prvi, drugi] = [1, 2]//dobijemo varijable  prvi drugi koje  imaju vrijednosti
+const [prvi,drugi] = [1,2] // dobijemo varijable prvi i drugi koje imaju vrijednost 1 i 2
 
 console.log(prvi, drugi)
 
-const { prezime, ...meniBitno } = polaznik
-//prezime  je ovdje  otpadak- ne treba  mi ali  mij  nacin da  iz  polaznika  maknem  prezime
-console.log(meniBitno)//imam ime  i edukacija
+const {prezime, ...meniBitno} = polaznik
+// prezime je ovdje otpadak - ne treba mi, ali mi je način da iz polaznika maknem prezime
 
-//nullish calescing operator
-//ako je vrijednost null
+console.log(meniBitno) // imam ime i edukacija, bez prezimena
+
+// nullish calescing operator (??)
+// ako je vrijednost varijable null ili undefined koristi nešto drugo
+
 let sifra
 
-console.log(sifra ?? 'Sifra nije postavljena')
-sifra = 1
-console.log(sifra ?? 'Sifra je postavljena')
+console.log(sifra ?? 'Šifra nije postavljena')
 
-//optional chaining operator
+sifra=1
 
-const korisnik{
+console.log(sifra ?? 'Šifra nije postavljena')
 
-  adresa: {
-    
-    grad:osijek
-  }
+
+// optional chaining operater ?.
+
+// idemo vidjeti što radi točka (.)
+
+const korisnik = {
+    adresa: {
+        grad: 'Osijek',
+        ulica: 'Trg'
+    }
 }
 
-console.log(korisnik.adresa.grad)//osijek
+console.log(korisnik.adresa.grad) // Osijek
 
-console.log(korisnik?.adresa?.ulica?.toUpperCase()?? 'Nije  postavljeno')
+console.log(korisnik?.adresa?.ulica?.toUpperCase() ?? 'Nije postavljeno')
