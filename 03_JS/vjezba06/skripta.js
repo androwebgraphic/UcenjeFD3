50fa40bf6f9b71624ac75c42ab5ecf77b31cf723
andreas <andreassklizovic@gmail.com>
2026-07-21 17:41:21 +0200

    switch uvjetno granaje

---
+++ 03_JS/vjezba06/skripta.js
@@ -0,0 +1,51 @@
+const ocjena = 2
+
+switch (ocjena) {
+  
+  case 1:
+    console.log('Nedovoljan')
+    break
+  
+  case 2:
+
+    console.log(' Dovoljan')
+      break
+  
+  case 3:
+    console.log('Dobar')
+    break
+  
+  case 4: 
+    console.log('Vrlo dobar')
+    break
+ 
+  case 5:
+    console.log('Izvrstan')
+    break
+  
+  default:
+    console.log(' Nije ocjena')
+
+}
+
+const dan = 'Subota'
+
+
+switch (dan) {
+  
+  case 'Ponedjeljak':
+  case 'Utorak':
+  case 'Srijeda':
+  case 'Cetvrtak':
+  case 'Petak':
+    console.log('Radni dan')
+    break
+  case 'Subota':
+  case 'Nedjelja':
+    console.log('Vikend')
+    break
+  default:
+    console.log('Neispravan dan')
+}
+
+
