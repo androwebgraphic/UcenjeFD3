1f270647936a9e46e90b5fe734d0455f6df7919f
andreas <andreassklizovic@gmail.com>
2026-07-30 18:52:52 +0200

    vjezbe

---
+++ 03_JS/vjezba12/app.js
@@ -0,0 +1,143 @@
+
+
+const zadaci = {
+  z1: (a, b) => {//1. Ispisi apsolutnu vrijednost A-B
+    console.log('Zadatak 1');
+    // početak 1
+    rezultat.innerHTML = ''
+    if (!a.trim()) {
+
+      rezultat.innerHTML = ' Polje "A" ne  moze  biti prazno'
+    }
+
+    if (!b.trim()) {
+
+      rezultat.innerHTML = ' Polje "B" ne  moze  biti prazno'
+    }
+
+    const Broja = Number(a)
+    const Brojb = Number(b)
+    if (!Broja) {
+
+      rezultat.innerHTML = ' a nije  broj'
+      return
+    }
+    if (!Brojb) {
+
+      rezultat.innerHTML = 'b nije  broj'
+      return
+    }
+    // const razlika = Broja-Brojb
+
+    //     if (razlika < 0) {
+
+    //       rezultat.innerHTML = razlika * -1
+    //     } else {
+    //       rezultat.innerHTML = razlika
+    // }
+    rezultat.innerHTML = Math.abs(Broja - Brojb)
+    // kraj 1
+  },
+  z2: (a, b) => {
+    console.log('Zadatak 2');
+    // početak 2
+    rezultat.innerHTML = ''
+
+
+    if (a.trim() == '' || b.trim() == '') {
+
+      rezultat.innerHTML = 'Unesi brojeve'
+      return
+    }
+    const brojA = Number(a)
+    const brojB = Number(b)
+    if (!brojA) {
+      rezultat.innerHTML = ' A nije broj'
+      return
+    }
+    if (!brojB) {
+
+      rezultat.innerHTML = 'B nije broj'
+      return
+    }
+    if (brojA === brojB) {
+      
+      rezultat.innerHTML = 'Broj a  i Broj b su jednaki'
+    }else if (brojA > brojB) {
+      rezultat.innerHTML = brojA
+    } else {
+
+      rezultat.innerHTML = brojB
+    }
+    // kraj 2
+  },
+  z3: (a, b) => {
+    console.log('Zadatak 3');
+    // početak 3 
+    if (a.trim() === '' || b.trim() === '') {
+      
+      rezultat.innerHTML = 'Unesite vrijednosti'
+      return
+    }
+
+    const brojA = Number(a)
+    if (!brojA) {
+      
+      rezultat.innerHTML = ' A nije broj'
+      return
+    }
+    if (brojA < 1 || brojA>100) {
+  
+      rezultat.innerHTML = 'Broj ne smije  biti negativan i veci od 100'
+      return
+}
+    let divovi = ''
+    for (let i = 0; i < brojA; i++){
+
+      if (i % 2 === 0) {
+        
+        divovi += `
+        <div style="background-color : ${b}">&nbsp;</div>
+        `
+      } else {
+        
+        divovi += '<div>&nbsp;</div>'
+      }
+
+
+    }
+
+    rezultat.innerHTML = divovi
+console.log(brojA, b)
+
+    // kraj 3
+  },
+  z4: (a, b) => {
+    console.log('Zadatak 4');
+    // početak 4
+
+    // kraj 4
+  },
+  z5: (a, b) => {
+    console.log('Zadatak 5');
+    // početak 5
+
+    // kraj 5
+  }
+  // ovdje dodajemo nove zadatke
+}
+
+
+const rezultat = document.getElementById('rezultat');
+document.getElementById('izvedi').addEventListener('click', () => {
+
+  const a = document.getElementById('a').value;
+  const b = document.getElementById('b').value;
+  const zadatak = document.getElementById('zadatak').value;
+  if (!(('z' + zadatak) in zadaci)) {
+    rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
+    return;
+  }
+  // poziv funkcije u objektu
+  zadaci['z' + zadatak](a, b);
+});
