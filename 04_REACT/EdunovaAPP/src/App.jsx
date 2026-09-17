6d3849f025b596ed28f7b114f42bf7be5f5c9245
androwebgraphic <andreassklizovic@gmail.com>
2026-09-17 18:51:55 +0200

    Uvoz bootstrapa u app

--- 04_REACT/EdunovaAPP/src/App.jsx
+++ 04_REACT/EdunovaAPP/src/App.jsx
@@ -1,14 +1,19 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { Badge, Container } from 'react-bootstrap'
 
 function App() {
 
   return (
    
-    <>
-    
-    Hello
-    </>
+    <Container>
+
+      Hello
+      <Badge bg='success'>Znacka</Badge>
+      <hr />
+      
+      <div className='podrucje'>Text</div>
+    </Container>
 
   
    
