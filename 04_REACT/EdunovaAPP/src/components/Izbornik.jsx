2b59ab4cb9f8d28e2d31c38bb74e51e680b5bf42
androwebgraphic <andreassklizovic@gmail.com>
2026-09-23 17:15:53 +0200

    components

--- 04_REACT/EdunovaAPP/src/components/Izbornik.jsx
+++ 04_REACT/EdunovaAPP/src/components/Izbornik.jsx
@@ -5,12 +5,9 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { IME_APLIKACIJE, RouteNames } from '../constants';
 import { useNavigate } from 'react-router-dom';
 
-
-
-
 export default function Izbornik() {
 
-const navigate = useNavigate()
+    const navigate = useNavigate()
 
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
@@ -21,13 +18,13 @@ const navigate = useNavigate()
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-              <Nav.Link
-                onClick={() => { navigate(RouteNames.HOME)}}
-              >Početna</Nav.Link>
+                        <Nav.Link 
+                        onClick={()=>{navigate(RouteNames.HOME)}}
+                        >Početna</Nav.Link>
                         <NavDropdown title="Programi" id="basic-nav-dropdown">
-                <NavDropdown.Item
-                         onClick={() => { navigate(RouteNames.SMJEROVI)}}
-                >Smjerovi</NavDropdown.Item>
+                            <NavDropdown.Item
+                            onClick={()=>navigate(RouteNames.SMJEROVI)}
+                            >Smjerovi</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
